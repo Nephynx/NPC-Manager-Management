78bfdcbaad6253c0a68f9bac15de144a18d713ae
JakIsSky <donedaabner@gmail.com>
2025-12-01 18:45:04 +0800

    commit

--- NPC Manager Management/AddUserForm.Designer.cs
+++ NPC Manager Management/AddUserForm.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             buttonClear = new Button();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             SuspendLayout();
             // 
             // buttonClear
@@ -40,19 +47,95 @@
             buttonClear.Text = "CREATE";
             buttonClear.UseVisualStyleBackColor = true;
             // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(292, 181);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(190, 23);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(292, 145);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(175, 23);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(292, 116);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 13;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(199, 184);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 17);
+            label4.TabIndex = 12;
+            label4.Text = "Password:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(199, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 17);
+            label3.TabIndex = 11;
+            label3.Text = "Username:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(199, 124);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 17);
+            label2.TabIndex = 10;
+            label2.Text = "Email:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(343, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 32);
+            label1.TabIndex = 9;
+            label1.Text = "Register";
+            // 
             // AddUserForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(buttonClear);
             Name = "AddUserForm";
             Text = "AddUserfrom";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button buttonClear;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
