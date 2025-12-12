6d2d7a740c3af6c3936369a9dc42e34fb503fe9b
Nephynx <saxtonjl58@gmail.com>
2025-12-12 12:25:41 +0800

    COMMIT AGAIN

--- NPC Manager Management/NPCManager.Designer.cs
+++ NPC Manager Management/NPCManager.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             ButtonNPCManager = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             SuspendLayout();
             // 
             // ButtonNPCManager
@@ -36,16 +40,60 @@
             ButtonNPCManager.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             ButtonNPCManager.Location = new Point(12, 12);
             ButtonNPCManager.Name = "ButtonNPCManager";
-            ButtonNPCManager.Size = new Size(133, 87);
+            ButtonNPCManager.Size = new Size(263, 87);
             ButtonNPCManager.TabIndex = 1;
             ButtonNPCManager.Text = "NPC Manager";
             ButtonNPCManager.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(281, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(263, 87);
+            button1.TabIndex = 1;
+            button1.Text = "NPC Manager";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(550, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(263, 87);
+            button2.TabIndex = 1;
+            button2.Text = "NPC Manager";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(819, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(263, 87);
+            button3.TabIndex = 1;
+            button3.Text = "NPC Manager";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(1088, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(263, 87);
+            button4.TabIndex = 1;
+            button4.Text = "NPC Manager";
+            button4.UseVisualStyleBackColor = true;
             // 
             // NPCManager
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1448, 636);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(ButtonNPCManager);
             Name = "NPCManager";
             Text = "NPCManager";
@@ -55,5 +103,9 @@
         #endregion
 
         private Button ButtonNPCManager;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
