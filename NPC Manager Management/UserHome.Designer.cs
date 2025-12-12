54462043f136e6cf2d561965cdb937f7df9fd999
Nephynx <saxtonjl58@gmail.com>
2025-12-12 12:29:43 +0800

    HOME

--- NPC Manager Management/UserHome.Designer.cs
+++ NPC Manager Management/UserHome.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
             button2 = new Button();
             label4 = new Label();
             label5 = new Label();
@@ -37,31 +36,13 @@
             button7 = new Button();
             button8 = new Button();
             button9 = new Button();
-            dataGridView1 = new DataGridView();
-            ID = new DataGridViewTextBoxColumn();
-            NPC = new DataGridViewTextBoxColumn();
-            PLAYER = new DataGridViewTextBoxColumn();
-            DIALOGUE = new DataGridViewTextBoxColumn();
-            label2 = new Label();
-            label3 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            button1 = new Button();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = SystemColors.ActiveCaption;
-            label1.Font = new Font("Bahnschrift Condensed", 50.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 7);
-            label1.Name = "label1";
-            label1.Size = new Size(539, 81);
-            label1.TabIndex = 1;
-            label1.Text = "Welcome to Home Page";
             // 
             // button2
             // 
             button2.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button2.Location = new Point(0, 164);
+            button2.Location = new Point(568, 7);
             button2.Name = "button2";
             button2.Size = new Size(133, 87);
             button2.TabIndex = 0;
@@ -95,7 +76,7 @@
             label8.AutoSize = true;
             label8.BackColor = Color.Silver;
             label8.Font = new Font("Segoe UI", 300F);
-            label8.Location = new Point(139, 156);
+            label8.Location = new Point(225, 143);
             label8.Name = "label8";
             label8.Size = new Size(1212, 532);
             label8.TabIndex = 6;
@@ -142,67 +123,23 @@
             button9.Text = "NPC Manager";
             button9.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // button1
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { ID, NPC, PLAYER, DIALOGUE });
-            dataGridView1.Location = new Point(148, 164);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(565, 459);
-            dataGridView1.TabIndex = 7;
-            // 
-            // ID
-            // 
-            ID.HeaderText = "ID";
-            ID.Name = "ID";
-            // 
-            // NPC
-            // 
-            NPC.HeaderText = "Non-Player Character";
-            NPC.Name = "NPC";
-            // 
-            // PLAYER
-            // 
-            PLAYER.HeaderText = "Player";
-            PLAYER.Name = "PLAYER";
-            // 
-            // DIALOGUE
-            // 
-            DIALOGUE.HeaderText = "Dialogue";
-            DIALOGUE.Name = "DIALOGUE";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.DarkGray;
-            label2.Font = new Font("Segoe UI", 150F);
-            label2.Location = new Point(723, 164);
-            label2.Name = "label2";
-            label2.Size = new Size(332, 265);
-            label2.TabIndex = 8;
-            label2.Text = "    ";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.BackColor = Color.DarkGray;
-            label3.Font = new Font("Segoe UI", 150F);
-            label3.Location = new Point(723, 355);
-            label3.Name = "label3";
-            label3.Size = new Size(332, 265);
-            label3.TabIndex = 9;
-            label3.Text = "    ";
+            button1.Font = new Font("Bahnschrift", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(550, 82);
+            button1.TabIndex = 7;
+            button1.Text = "Welcome To NPC Dialogue Manager";
+            button1.UseVisualStyleBackColor = true;
             // 
             // UserHome
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Desktop;
-            ClientSize = new Size(1069, 633);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(dataGridView1);
-            Controls.Add(label1);
+            ClientSize = new Size(1449, 641);
+            Controls.Add(button1);
             Controls.Add(button9);
             Controls.Add(button8);
             Controls.Add(button7);
@@ -216,13 +153,11 @@
             RightToLeftLayout = true;
             Text = "UserHome";
             Load += UserHome_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-        private Label label1;
         private Button button2;
         private Label label4;
         private Label label5;
@@ -231,12 +166,6 @@
         private Button button7;
         private Button button8;
         private Button button9;
-        private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn ID;
-        private DataGridViewTextBoxColumn NPC;
-        private DataGridViewTextBoxColumn PLAYER;
-        private DataGridViewTextBoxColumn DIALOGUE;
-        private Label label2;
-        private Label label3;
+        private Button button1;
     }
 }
